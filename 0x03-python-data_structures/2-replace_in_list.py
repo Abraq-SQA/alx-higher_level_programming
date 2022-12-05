1649432b46c4e6cafb90a007ec42ef6bcf569300
Abraq-SQA <johnabrak01@gmail.com>
2022-12-04 23:34:57 -0800

    replace element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    """Replace an element of a list at a specific position."""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return my_list
 
-if __name__ == "__main__":
-    """Print the number of and list of arguments."""
-    import sys
-
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    my_list[idx] = element
+    return my_list
